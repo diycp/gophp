23a4f50fbef20c282b0f5a790144ebb37ce16eb5
gouguoyin <gouguoyin@juzifenqi.com>
2017-05-12 23:12:02 +0800

    demo

---
+++ gophp/config/cookie.php
@@ -0,0 +1,14 @@
+<?php
+//默认配置
+return [
+
+    'token'  => '01CVhZIgSogB85WNhwYVk79A==',
+    'prefix' => 'gophp_',
+    'expire' => 24 * 3600,
+    'path'   => '/',
+    'domain' => '',
+    'secure' => '',
+
+];
+
+
