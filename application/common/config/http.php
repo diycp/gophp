68c149c06830a94c1ddb15675aa48bfaafe1b653
gouguoyin <gouguoyin@juzifenqi.com>
2017-05-22 00:37:57 +0800

    优化numbers()方法

--- application/common/config/http.php
+++ application/common/config/http.php
@@ -3,6 +3,7 @@
 return [
 
     'uri_param'          => 'r',
+    'page_param'         => 'p', // 分页参数
     'url_rewrite'        => true,  //是否开启url重写
     'default_module'     => DEFAULT_MODULE,  //默认模块
     'module_alias'       => ['admin' => 'manage'],  //模块别名
