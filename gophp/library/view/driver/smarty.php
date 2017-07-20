d4491b7a67117257c7cfcadd03abb34fd3999dfd
gouguoyin@juzifenqi.com <68823095>
2017-07-20 19:55:48 +0800

    默认关闭错误报告

--- gophp/library/view/driver/smarty.php
+++ gophp/library/view/driver/smarty.php
@@ -13,8 +13,6 @@ class smarty extends contract
 
         $this->config = $config['smarty'];
 
-        echo 'llll';
-
         $this->view = new \Smarty();
 
         $this->view->template_dir    = VIEW_PATH;
