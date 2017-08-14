373a46f3a5347cb34f0530937ce291b9a0e10bff
gouguoyin@juzifenqi.com <68823095>
2017-08-14 20:19:52 +0800

    db类join和on方法自动添加前缀，session支持数组格式存储

--- application/home/controller/index.php
+++ application/home/controller/index.php
@@ -12,7 +12,7 @@ class index extends controller {
 
     public function index(){
 
-        db::instance()->table('kkk', 'go_');
+        db::instance()->table('kkk', '');
 
 
         $this->display();
