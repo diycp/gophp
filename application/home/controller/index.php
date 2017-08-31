d11aefab787b34ee7ce8b4bd67d2df87d7c42ca4
够过瘾 <245629560@qq.com>
2017-08-31 20:03:19 +0800

    IDE完美支持智能提示

--- application/home/controller/index.php
+++ application/home/controller/index.php
@@ -7,37 +7,28 @@ use gophp\db;
 use gophp\mail;
 use gophp\request;
 use gophp\route;
+use Workerman\Worker;
 
 
 class index extends controller {
 
     public function index(){
 
-        $mail = config('mail' );
+        $task = new Worker();
+        $task->count = 1;
 
-        $a = new mail\driver\phpmailer($mail);
+        $task->onWorkerStart = function($task)
+        {
+            // 每2.5秒执行一次
+            $time_interval = 2.5;
+            Timer::add($time_interval, function()
+            {
+                echo "task run\n";
+            });
+        };
 
-        $b = $a->to('245629560@qq.com')->title('kkk')->body('cesss')->send();
-
-        dump($b);
+        Worker::runAll();
 
     }
-
-    private function httpGet($url) {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-        // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
-        // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
-        curl_setopt($curl, CURLOPT_URL, $url);
-
-        $res = curl_exec($curl);
-        curl_close($curl);
-
-        return $res;
-    }
-
 
 }
