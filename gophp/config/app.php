55df8749dd4c39891d733cc450debd43c8810f4b
够过瘾 <245629560@qq.com>
2017-09-03 21:12:07 +0800

    redis配置文件

--- gophp/config/app.php
+++ gophp/config/app.php
@@ -2,9 +2,9 @@
 //默认配置
 return [
 
-    'app_namespace'     => 'app',
-    'default_timezone'  => 'PRC',   //默认时区
-    'default_charset'   => 'utf-8', //默认字符编码
+    'app_namespace'     => 'app', // 应用命名空间
+    'default_timezone'  => 'PRC', // 默认时区
+    'default_charset'   => 'utf-8', // 默认字符编码
 
 ];
 
